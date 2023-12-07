0422af2e7555cbd5c29f9464acddabd2c7b0ddc5
CChirino <34484098+CChirino@users.noreply.github.com>
2023-12-07 12:53:06 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>Welcome!</h1>
+    <!-- Type here your code -->
+  </Layout>
+</template>
+
+<script lang="ts">
+import Layout from './layouts/default.vue';
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { Layout },
+});
+</script>
